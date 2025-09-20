237578f3eb5dee6bbfc39c05bfae8ce9ca5bfe6c
Mayank <awf>
2025-09-21 00:37:16 +0530

    Day2

---
+++ Day2/day2.swift
@@ -0,0 +1,21 @@
+import Foundation 
+var name: String = "Mayank"
+var age: Int=22
+var height: Double = 5.9
+var isStudent Bool = true
+//Constants (are immutable)
+
+let country: String = "India"
+
+let pi: Doouble = 3.14
+
+//Printing Values
+print("Name: \(name)")
+print("Age: \(age)")
+print("Height: \(height)")
+print("Is Student: \(isStudent)")
+
+name = "Maya"
+age="22"
+print("Name: \(name)")
+print("Age: \(age)")
